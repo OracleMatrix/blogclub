4094fee258bb1af6deb22f0c422dceaf53ba95fd
Ehsan <ehsanmohamadipoor@gmail.com>
2024-08-07 02:05:35 +0330

    onBoardingScreen page

---
+++ lib/onboarding.dart
@@ -0,0 +1,154 @@
+import 'package:blogclub/data.dart';
+import 'package:blogclub/gen/assets.gen.dart';
+import 'package:blogclub/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  final items = AppDatabase.onBoardingItems;
+  int page = 0;
+
+  @override
+  void initState() {
+    _pageController.addListener(
+      () {
+        if (_pageController.page!.round() != page) {
+          setState(() {
+            page = _pageController.page!.round();
+          });
+        }
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      backgroundColor: themeData.colorScheme.surface,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32.0, bottom: 32),
+                child: Assets.img.background.onboarding.image(),
+              ),
+            ),
+            Container(
+              height: 280,
+              decoration: BoxDecoration(
+                color: themeData.colorScheme.onSurface,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 20,
+                    color: Colors.black.withOpacity(0.1),
+                  )
+                ],
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: PageView.builder(
+                      itemCount: items.length,
+                      controller: _pageController,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(32.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                items[index].title,
+                                style: themeData.textTheme.headlineMedium,
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Text(
+                                items[index].description,
+                                style: themeData.textTheme.titleLarge!.apply(
+                                  fontSizeFactor: 0.9,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        const EdgeInsets.only(left: 32, right: 32, bottom: 8),
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SmoothPageIndicator(
+                          controller: _pageController,
+                          count: items.length,
+                          effect: ExpandingDotsEffect(
+                            dotHeight: 8,
+                            dotWidth: 8,
+                            activeDotColor: themeData.colorScheme.primary,
+                            dotColor:
+                                themeData.colorScheme.primary.withOpacity(0.1),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (page == items.length - 1) {
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => const HomeScreen(),
+                                ),
+                              );
+                            } else {
+                              _pageController.animateToPage(page + 1,
+                                  duration: const Duration(milliseconds: 500),
+                                  curve: Curves.decelerate);
+                            }
+                          },
+                          style: ButtonStyle(
+                            minimumSize: WidgetStateProperty.all(
+                              const Size(84, 60),
+                            ),
+                            shape: WidgetStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                          child: Icon(
+                            page == items.length - 1
+                                ? CupertinoIcons.check_mark
+                                : CupertinoIcons.arrow_right,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
