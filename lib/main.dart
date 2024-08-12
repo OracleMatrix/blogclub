1adc1ac9fd0d610280c5bdfa3b81ebfe4fb8bd4c
Ehsan <ehsanmohamadipoor@gmail.com>
2024-08-12 17:46:53 +0330

    BlogClub last update!

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:blogclub/article.dart';
 import 'package:blogclub/gen/fonts.gen.dart';
 import 'package:blogclub/home.dart';
 import 'package:blogclub/profile.dart';
+import 'package:blogclub/splash.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -87,7 +88,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: false,
       ),
-      home: const MainScreen(),
+      home: const SplashScreen(),
     );
   }
 }
